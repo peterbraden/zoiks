2387ba88eca0999229c4d6388eb50f103b1cb112
Peter Braden <pbraden@yammer-inc.com>
2011-01-20 13:02:09 -0800

    Lookup file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var sys = require('sys')
   , http = require('http')
+  , fs = require('fs')
   , url = require('url')
   , path = require('path')
   , scriptTools = require('scriptTools')
@@ -8,6 +9,8 @@ var sys = require('sys')
 var opts = scriptTools.optParse(process.argv.slice(2))  
   , PORT = opts[0]['-p'] || 80
   , DEBUGLEVEL = opts[0]['--debug']
+  , DIR = opts[1][0] || __dirname
+  , URLPREFIX = opts[0]['--prefix'] || '/'
 
 
 var debug = {
@@ -70,7 +73,11 @@ http.createServer(function(request, response){
   
   // Parse URL
   try{
-    paths = parsePath(url.parse(request.url).pathname)
+    var pathname = url.parse(request.url).pathname
+    if (pathname.indexOf(URLPREFIX) === 0)
+      pathname = pathname.substring(URLPREFIX.length)
+    
+    paths = parsePath(pathname)
   } catch (e){
     response.writeHead(500, {'Content-Type': 'text/plain'})
     response.end("Error:", e) 
@@ -80,6 +87,24 @@ http.createServer(function(request, response){
   //[TODO Versioning]
   
   // Lookup Files
+  var _f
+  try{
+    _.each(paths, function(path){
+      _f = DIR + path;
+      console.log(fs.readFileSync(fs.realpathSync(DIR + path), 'utf8'))
+    
+    })
+  } catch (e){
+    debug.warn("Couldn't find file: ", _f)
+    debug.log(e)
+    response.writeHead(404, {'Content-Type': 'text/html'})
+    response.write("Couldn't find script:" +  _f)
+    response.write("While serving:" + paths.join(" "))
+    response.end("Error:" + e) 
+    return;
+    
+  }  
+  
   
   //[TODO Dependencies?]
   
@@ -91,4 +116,4 @@ http.createServer(function(request, response){
   response.end("!")
   
 }).listen(PORT)
-debug.log("Started server, port:", PORT)
+debug.log("Started server, port:", PORT, " serving from:" , DIR)
